d5e25989231f4c1c4dfb9caf3aa9120db804e76c
Mehrnoush402 <Ebrahimi.M.9696@gmail.com>
2025-02-22 11:00:42 +0330

    refactor registration page to use separate RegisterForm component

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,101 +1,32 @@
-"use client";
-
-import { register } from "@/actions/register";
+import RegisterForm from "@/components/forms/RegisterForm";
 import {
   Card,
   CardContent,
   Stack,
   Typography,
   Link as MuiLink,
-  TextField,
-  Button,
-  Box,
 } from "@mui/material";
 import Link from "next/link";
-import React, { useActionState } from "react";
+
+import React from "react";
 
 export default function RegisterPage() {
-  const [state, action, pending] = useActionState(register, undefined);
+  
 
   return (
     <Card sx={{ width: 500 }} elevation={8}>
       <CardContent sx={{ padding: 4 }}>
-        <form action={action}>
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
-          >
-            <Typography variant="h5">ثبت نام</Typography>
-            <MuiLink component={Link} href="login">
-              قبلا ثبت نام کرده اید؟
-            </MuiLink>
-          </Stack>
-          <Stack gap={3}>
-            <Stack
-              mt={2}
-              direction="row"
-              alignItems="center"
-              justifyContent="space-between"
-              gap={1}
-            >
-              <TextField
-                error={!!state?.errors?.firstName}
-                helperText={state?.errors?.firstName}
-                name="firstName"
-                fullWidth
-                label="نام"
-                variant="outlined"
-                size="small"
-              />
-              <TextField
-                error={!!state?.errors?.lastName}
-                helperText={state?.errors?.lastName}
-                name="lastName"
-                fullWidth
-                label="نام خانوادگی"
-                variant="outlined"
-                size="small"
-              />
-            </Stack>
-            <TextField
-              error={!!state?.errors?.email}
-              helperText={state?.errors?.email}
-              name="email"
-              fullWidth
-              label="رایانامه"
-              variant="outlined"
-              size="small"
-            />
-            <TextField
-              error={!!state?.errors?.password}
-              helperText={state?.errors?.password?.map((err:string)=><Box component="span" display="block" key={err}>{err}</Box>)}
-              name="password"
-              fullWidth
-              label="کلمه عبور"
-              variant="outlined"
-              size="small"
-            />
-            <Typography variant="caption">
-              با ثبت نام در سرویس ما شمابا همه قوانین سرویس موافقت خود را اعلام
-              میدارید.
-            </Typography>
-            <Button
-              disableElevation
-              disabled={pending}
-              type="submit"
-              variant="contained"
-              sx={{
-                transition: "transform 0.2s ease-in-out",
-                "&:hover": {
-                  transform: "scale(1.01)",
-                },
-              }}
-            >
-              ثبت نام
-            </Button>
-          </Stack>
-        </form>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Typography variant="h5">ثبت نام</Typography>
+          <MuiLink component={Link} href="login">
+            قبلا ثبت نام کرده اید؟
+          </MuiLink>
+        </Stack>
+        <RegisterForm />
       </CardContent>
     </Card>
   );
