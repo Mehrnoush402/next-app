b5234892a2d5360fa04afea2cf163bc260fdaaf8
Mehrnoush402 <Ebrahimi.M.9696@gmail.com>
2025-02-27 13:34:17 +0330

    add dashboard layout components including Drawer, MiniDrawer, and Search elements

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,39 @@
+import DrawerHeader from "@/components/dashboard-layout/components/DrawerHeader";
+import DashboardHeader from "@/components/dashboard-layout/DashboardHeader";
+import MiniDrawer from "@/components/dashboard-layout/MiniDrawer";
+import { Box } from "@mui/material";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  return (
+    <Box sx={{ display: "flex" }}>
+      <DashboardHeader />
+      {/* <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={[
+              {
+                marginRight: 5,
+              },
+              open && { display: "none" },
+            ]}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Mini variant drawer
+          </Typography>
+        </Toolbar> */}
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+        <MiniDrawer />
+      </Box>
+    </Box>
+  );
 }
